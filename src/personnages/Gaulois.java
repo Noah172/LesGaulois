4eb13c0267310e30bfd246b60ddf39eaed074be4
Noah172 <114427265+Noah172@users.noreply.github.com>
2022-09-26 19:42:27 +0200

    TP1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,26 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public int getForce() {
+		return force;
+	}
+	public void setForce(int force) {
+		this.force = force;
+	}
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+	public Gaulois(String nom, int force, int effetPotion) {
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+public static void main(String[] args) {
+}}
