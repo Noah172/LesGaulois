e6ac1b9a037c5f3968ede51fbd6addbf1d00a0cb
Noah172 <114427265+Noah172@users.noreply.github.com>
2022-10-17 17:40:28 +0200

    TP2

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Equipement {
+	CASQUE("casque"),BOUCLIER("bouclier");
+	
+	private String nom;
+
+	private Equipement(String nom) {
+		this.nom = nom;
+		
+	}
+	public String() {
+		return nom;
+	}
+}
